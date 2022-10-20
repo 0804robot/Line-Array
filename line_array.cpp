67a804bec56e50cc5f7d08f0ff6d0c4e2b67644a
sammy <sammyoina@gmail.com>
2022-10-21 00:20:46 +0300

    f(main) add check for no infrared

--- line_array.cpp
+++ line_array.cpp
@@ -32,6 +32,8 @@ int LineArray::readValue()
     int pos = linePosition[0] * 4 + linePosition[1] * 3 + linePosition[2]  * 2 + linePosition[3] * 1 + linePosition[4] * 0;
     if (linePosition[0] == 0 && linePosition[1] == 0 && linePosition[2] ==  0 && linePosition[3] == 0 && linePosition[4] == 0){
         return previous_position;
+    }else if (linePosition[0] == 1 && linePosition[1] == 1 && linePosition[2] ==  1 && linePosition[3] == 1 && linePosition[4] == 1){
+        return previous_position;
     }
     pos = 2 - (pos /(linePosition[0] + linePosition[1] + linePosition[2] + linePosition[3] + linePosition[4]));
     previous_position = pos;
